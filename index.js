5eed7bbd06fd67fbe9e6a98f33a9cea72980e9cf
DavTheDev20 <reiddavin@outlook.com>
2021-08-28 12:56:52 -0400

    Fix content on smaller screens, create contact section, and fix styling

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+let footerCopyright = $(
+  `<p class="footer-copyright">© Davin Reid ${currentYear}</p>`
+);
+let githubLink =
+  '<a href="https://github.com/DavTheDev20"><i class="fab fa-github-square fa-2x"></i></a>';
+let linkedinLink =
+  '<a href="https://www.linkedin.com/in/davin-reid-9273071b9/"><i class="fab fa-linkedin fa-2x"></i></a>';
+let emailLink =
+  '<a href="mailto:reiddavin@outlook.com"><i class="fas fa-envelope-square fa-2x"></i></a>';
+let footerLinks = $(`<p>${githubLink} ${linkedinLink} ${emailLink}</p>`);
+
+$('footer').append(footerLinks);
+$('footer').append(footerCopyright);
+
+const nameInput = $('[name="name"]');
+const emailInput = $('[name="email"]');
+const messageInput = $('[name="message"]');
+
+function sendMail() {
+  const name = nameInput.val();
+  const email = emailInput.val();
+  const message = messageInput.val();
+
+  const subject = `Contact Message from ${name} (${email})`;
+
+  window.open(
+    `mailto:reiddavin@outlook.com?subject=${subject}&body=${message}`
+  );
+}
+
 $('.main-carousel').flickity({
   cellAlign: 'center',
   contatin: true,
